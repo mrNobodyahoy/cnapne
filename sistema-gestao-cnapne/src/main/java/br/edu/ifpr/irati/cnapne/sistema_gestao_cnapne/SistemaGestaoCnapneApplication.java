7ceca549539cd027afaf4e16f1650a1bea099932
mrNobodyahoy <prvidal00@gmail.com>
2025-08-10 16:42:58 -0300

    criacao do arquivo

---
+++ sistema-gestao-cnapne/src/main/java/br/edu/ifpr/irati/cnapne/sistema_gestao_cnapne/SistemaGestaoCnapneApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpr.irati.cnapne.sistema_gestao_cnapne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestaoCnapneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestaoCnapneApplication.class, args);
+	}
+
+}
---
+++ sistema-gestao-cnapne/src/test/java/br/edu/ifpr/irati/cnapne/sistema_gestao_cnapne/SistemaGestaoCnapneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpr.irati.cnapne.sistema_gestao_cnapne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestaoCnapneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
